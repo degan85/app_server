09048c8ee25f2cd71b84830a2053f1c013fc73c6
cho <degan8535@gmail.com>
2017-12-26 17:00:40 +0900

    first database test

--- src/main/java/com/ibk/app_server/controller/ASMainController.java
+++ src/main/java/com/ibk/app_server/controller/ASMainController.java
@@ -1,5 +1,11 @@
 package com.ibk.app_server.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -9,10 +15,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ibk.app_server.dao.User;
+
 @Controller
 @RequestMapping(value = "/")
 public class ASMainController {
 	Logger logger = LoggerFactory.getLogger(ASMainController.class);
+	
+	public Boolean selectTest() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/spring_db?verifyServerCertificate=false&amp;useSSL=false", "root","inno100");
+		
+		PreparedStatement ps = c.prepareStatement("select * from users");
+		
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			System.out.println(rs.getString("id"));
+			return true;
+		}
+		return false;
+	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String first() {
